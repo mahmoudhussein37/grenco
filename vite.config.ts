0998a0e6d33013fe9d78cd241ed37e958a03be6d
mahmoudhussein37 <hussienmahmoud34@gmail.com>
2025-05-29 02:59:47 +0300

    Configure deployment for grenco repository

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/grenco-website/', // Replace with your repository name
+  base: '/grenco/', // Replace with your repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
   }
 })
+
